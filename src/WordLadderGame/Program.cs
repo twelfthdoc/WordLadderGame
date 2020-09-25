4e8d01f029fbd6da8c420d398769e051b3eba1be
Luke Dawkin <luke.dawkin@hotmail.co.uk>
2020-09-25 17:14:53 +0100

    Rearranged solution structure; Added test project for Unit Tests

---
+++ src/WordLadderGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using WordLadderGame;
+
+namespace BluePrism_Technical_Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Startup.Initialize(args[0]);           
+            Startup.Run();
+            Startup.Close();
+        }
+    }
+}
---
+++ src/WordLadderGame/Startup.cs
@@ -0,0 +1,22 @@
+﻿namespace WordLadderGame
+{
+    public static class Startup
+    {
+        private const string DEFAULT_LOCATION = "./FileNameHere.txt";
+
+        public static void Initialize(string targetLocation)
+        {
+            targetLocation ??= DEFAULT_LOCATION;
+        }
+
+        public static void Run()
+        {
+
+        }
+
+        public static void Close()
+        {
+
+        }
+    }
+}
