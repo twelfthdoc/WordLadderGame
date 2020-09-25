3e550434c426909bceed0174cb0351012f886c46
Luke Dawkin <luke.dawkin@hotmail.co.uk>
2020-09-25 06:36:26 +0100

    Initial commit

---
+++ BluePrism Technical Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BluePrism_Technical_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
