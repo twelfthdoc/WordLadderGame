934618c13c22463e87348b85a919eeee0075ff6e
Luke Dawkin <luke.dawkin@hotmail.co.uk>
2020-09-27 19:05:57 +0100

    Added IsAlpha Helper Method

--- src/WordLadderGame/Common/HelperMethods.cs
+++ src/WordLadderGame/Common/HelperMethods.cs
@@ -5,6 +5,8 @@ namespace WordLadderGame.Common
 {
     public static class HelperMethods
     {
+        public static bool IsAlpha(this string s) => s.All(char.IsLetter);
+
         public static bool IsNullOrEmpty(this IEnumerable<object> enumerable) => enumerable == null || !enumerable.Any();
     }
 }
--- test/WordLadderGameTests/Common/HelperMethodsTests.cs
+++ test/WordLadderGameTests/Common/HelperMethodsTests.cs
@@ -7,6 +7,110 @@ namespace WordLadderGameTests.Common
 {
     public class HelperMethodsTests
     {
+        #region IsAlpha Tests
+        [Theory]
+        [InlineData(@"")]
+        [InlineData(@"string")]
+        public void IsAlphaReturnsTrueWhenStringIsAlphabetic(string sut)
+        {
+            Exception exception = null;
+            bool result = false;
+
+            try
+            {
+                result = sut.IsAlpha();
+            }
+            catch(Exception e)
+            {
+                exception = e;
+            }
+
+            Assert.NotNull(sut);
+            Assert.Null(exception);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsAlphaReturnsFalseWhenStringHasNumbers()
+        {
+            var sut = @"12345";
+            Exception exception = null;
+            bool result = true;
+
+            try
+            {
+                result = sut.IsAlpha();
+            }
+            catch (Exception e)
+            {
+                exception = e;
+            }
+
+            Assert.NotNull(sut);
+            Assert.Null(exception);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsAlphaReturnsFalseWhenStringHasPunctuation()
+        {
+            var sut = @"!$%^";
+            Exception exception = null;
+            bool result = true;
+
+            try
+            {
+                result = sut.IsAlpha();
+            }
+            catch (Exception e)
+            {
+                exception = e;
+            }
+
+            Assert.NotNull(sut);
+            Assert.Null(exception);
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsAlphaThrowsWhenStringIsNull()
+        {
+            string sut = null;
+            Exception exception = null;
+            
+            try
+            {
+                sut.IsAlpha();
+            }
+            catch (Exception e)
+            {
+                exception = e;
+            }
+
+            Assert.Null(sut);
+            Assert.NotNull(exception);
+        }
+
+        [Fact]
+        public void IsAlphaThrowsWhenObjectIsNotString()
+        {
+            dynamic sut = new List<int>();
+            Exception exception = null;
+            
+            try
+            {
+                HelperMethods.IsAlpha(sut);
+            }
+            catch (Exception e)
+            {
+                exception = e;
+            }
+
+            Assert.NotNull(sut);
+            Assert.NotNull(exception);
+        }
+        #endregion
+
         #region IsNullOrEmpty Tests
         [Fact]
         public void IsNullOrEmptyReturnsTrueWhenObjectIsNull()
